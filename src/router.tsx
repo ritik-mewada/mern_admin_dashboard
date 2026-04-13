0641cbe72641a922fceb01d3c65e6a9649f7b90a
ritik-mewada <ritikmewada@gmail.com>
2026-04-13 18:05:03 -0400

    first componet

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/login/login.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/auth/login",
+    element: <LoginPage />,
   },
 ]);
